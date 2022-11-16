fa5ec9c7871e0ea00398e643c304eed1989c405e
Julian Archila <julianarchilacaro@gmail.com>
2022-11-15 20:46:44 -0500

    minor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+import { AABB, Box2d, Circle, Line } from "./primitives";
 class Engine {
   constructor() {
     const display = new Path2D(); // todo
   }
+
+  primitives = {
+    AABB,
+    Box2d,
+    Circle,
+    Line,
+  };
 }
 
 const canvas = document.getElementById("canvas");
--- src/primitives/index.js
+++ src/primitives/index.js
@@ -3,9 +3,4 @@ import Line from "./Line";
 import AABB from "./AABB";
 import Box2d from "./Box2d";
 
-export default {
-  Circle,
-  Line,
-  AABB,
-  Box2d,
-};
+export { Circle, Line, AABB, Box2d };
