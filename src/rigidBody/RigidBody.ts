a5b3447a379c8c3945fc4bacba3e23f37e0611e3
David Daza <99994731+DavidDaza2906@users.noreply.github.com>
2022-11-27 00:27:51 -0500

    Update RigidBody.ts

--- src/rigidBody/RigidBody.ts
+++ src/rigidBody/RigidBody.ts
@@ -3,8 +3,22 @@ import { Vector } from "../utils/vector";
 class Rigidbody {
   position: Vector = new Vector();
   rotation: number = 0.0;
+  linearVelocity : Vector = new Vector();
+  angularVelocity : number = 0.0;
+  linearDamping : number = 0.0;
+  angularDamping : number = 0.0;
+  fixedRotation : boolean = false;
+  getPosition(){
+   return this.position;
+  }
+  setTransform(position: Vector, rotation : number){
+    this.position = position;
+    this.rotation = rotation;
 
-  Rigidbody() {}
+  }
+  getRotaion(){
+    return this.rotation;
+  }
 }
 
 export default Rigidbody;
